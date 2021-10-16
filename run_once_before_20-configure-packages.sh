c423f9f25a6479aa436453243b41cec7db1b2c16
Josh Kasuboski <josh.kasuboski@gmail.com>
2021-10-16 16:14:58 -0500

    add doppler

--- run_once_before_20-configure-packages.sh
+++ run_once_before_20-configure-packages.sh
@@ -6,3 +6,4 @@ nvim -c "PlugInstall | :qa"
 pipx install pyinfra
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+(curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh || wget -t 3 -qO- https://cli.doppler.com/install.sh) | sh -s -- --verify-signature
