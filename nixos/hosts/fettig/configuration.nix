e676e022ef3de728035463b8e3c5cb3d03785e25
Josh Kasuboski <josh.kasuboski@gmail.com>
2023-06-02 19:22:04 +0000

    cachix and more

--- nixos/hosts/fettig/configuration.nix
+++ nixos/hosts/fettig/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./cachix.nix
       ./ephemeral.nix
       ../../users/josh
     ];
--- nixos/users/josh/home.nix
+++ nixos/users/josh/home.nix
@@ -4,11 +4,36 @@
     stateVersion = "23.05";
     username = "josh";
     homeDirectory = "/home/${config.home.username}";
-  };    
+  };
+
+  xdg.enable = true;
+     
   programs = {
     home-manager.enable = true;
     git.enable = true;
+    bat.enable = true;
     direnv.enable = true;
     fish.enable = true;
+    fzf.enable = true;
+    lsd = {
+      enableAliases = true;
+    };
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+    starship = {
+      enable = true;
+      settings = lib.importTOML ../../../dot_config/starship.toml;
+    };
+    zoxide.enable = true;
   };
+
+  home.packages = with pkgs; [
+    fd
+    jq
+    yq
+  ];
 }
