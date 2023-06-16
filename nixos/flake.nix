9a17da0b0cef7a7878c1677066ee0a293ab8065e
Josh Kasuboski <josh.kasuboski@gmail.com>
2023-06-16 19:17:59 +0000

    move fettig flake to top

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,25 +2,58 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     lima.url = "github:kasuboski/nixos-lima";
+    impermanence.url = "github:Nix-community/impermanence";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+    vscode-server.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, nixpkgs, flake-utils, lima, ... }@attrs:
-    {
-      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+
+  outputs = {
+    nixpkgs,
+    lima,
+    impermanence,
+    home-manager,
+    vscode-server,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux"];
+    forEachPkgs = f: forEachSystem (sys: f nixpkgs.legacyPackages.${sys});
+  in {
+    formatter = forEachPkgs (pkgs: pkgs.alejandra);
+    nixosConfigurations = {
+      fettig = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs;};
+        modules = [
+          impermanence.nixosModule
+          ./hosts/fettig/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.extraSpecialArgs = {inherit inputs;};
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+        ];
+      };
+
+      nixos = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
-        specialArgs = attrs;
+        specialArgs = {inherit inputs;};
         modules = [
           lima.nixosModules.lima
           ./user-config.nix
         ];
       };
 
-      nixosConfigurations.nixosx86 = nixpkgs.lib.nixosSystem {
+      nixosx86 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-        specialArgs = attrs;
+        specialArgs = {inherit inputs;};
         modules = [
           lima.nixosModules.lima
           ./user-config.nix
         ];
       };
     };
+  };
 }
