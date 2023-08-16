eef58583e3966ad224ed55bfa8440693ee0d6f49
Josh Kasuboski <josh.kasuboski@gmail.com>
2023-08-16 15:47:27 +0000

    clientlookup that doesn't matter

--- nixos/hosts/ziel/blocky.nix
+++ nixos/hosts/ziel/blocky.nix
@@ -11,6 +11,23 @@
           "https://doh.opendns.com/dns-query"
         ];
       };
+      bootstrapDns = [
+        {
+          upstream = "https://doh.opendns.com/dns-query";
+          ips = [
+            "146.112.41.2"
+          ];
+        }
+        {
+          upstream = "https://1.1.1.1/dns-query";
+        }
+        {
+          upstream = "https://1.0.0.1/dns-query";
+        }
+      ];
+      clientLookup = {
+        upstream = "192.168.86.1";
+      };
       caching = {
         minTime = "5m";
         maxTime = "30m";
