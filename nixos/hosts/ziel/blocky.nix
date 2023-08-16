98722e6a2d793e07b4064b34d31202e2b5ef4075
Josh Kasuboski <josh.kasuboski@gmail.com>
2023-08-16 16:55:35 +0000

    enable blocky rest api

--- nixos/hosts/ziel/blocky.nix
+++ nixos/hosts/ziel/blocky.nix
@@ -5,6 +5,19 @@
   services.blocky = {
     enable = true;
     settings = {
+      blocking = {
+        blackLists = {
+          light = [
+            "https://raw.githubusercontent.com/hagezi/dns-blocklists/main/domains/light.txt"
+          ];
+        };
+        clientGroupsBlock = {
+          default = [
+            "light"
+          ];
+        };
+        startStrategy = "fast";
+      };
       upstream = {
         default = [
           "https://cloudflare-dns.com/dns-query"
@@ -32,6 +45,7 @@
         minTime = "5m";
         maxTime = "30m";
       };
+      ports.http = ":4000";
     };
   };
 }
