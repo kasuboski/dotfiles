fef333109ae32ef7b69699a4f8fbb0a5787e704a
Josh Kasuboski <josh.kasuboski@gmail.com>
2024-08-27 18:58:21 +0000

    install docker for linux

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+install_nix() {
+  # yolo
+  curl -sSf -L https://install.lix.systems/lix | sh -s -- install
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+}
+
+install_docker() {
+  curl -fsSL https://get.docker.com | sudo sh -s
+  curr="$USER"
+  sudo adduser "$curr" docker
+  newgrp docker
+}
+
 os=$(uname -s)
 # if nixos
 if uname -v | grep NixOS 2> /dev/null; then
@@ -12,13 +25,16 @@ elif [ "$os" = "Linux" ]; then
   echo "Detected Linux"
   if ! command -v nix > /dev/null; then
     echo "No Nix found; Installing Lix"
-    # yolo
-    curl -sSf -L https://install.lix.systems/lix | sh -s -- install
-    . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+    install_nix
   fi
 
   arch=$(uname -m | cut -d '_' -f 1)
   nix run --refresh home-manager/master -- switch --flake "github:kasuboski/dotfiles#$USER@$arch"
+
+  if ! command -v docker > /dev/null; then
+    echo "Installing Docker"
+    install_docker
+  fi
 else
   echo "Unknown machine"
   return 1
