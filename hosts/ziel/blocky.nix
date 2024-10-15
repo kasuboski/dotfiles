d3f9c124601013a570c8307d18256d2b2a195fd8
Josh Kasuboski <josh.kasuboski@gmail.com>
2024-10-15 19:30:14 +0000

    blocky

--- hosts/ziel/blocky.nix
+++ hosts/ziel/blocky.nix
@@ -1,9 +1,28 @@
-{...}: {
+{...}: let
+  routerIp = "192.168.86.1";
+in {
   networking.firewall.allowedTCPPorts = [53];
   networking.firewall.allowedUDPPorts = [53];
   services.blocky = {
     enable = true;
     settings = {
+      customDNS = {
+        mapping = {
+          "joshcorp.co" = "192.168.86.240";
+        };
+      };
+      conditional = {
+        fallbackUpstream = true;
+        rewrite = {
+          home = "lan";
+        };
+        mapping = {
+          "joshcorp.co" = "127.0.0.1";
+          lan = routerIp;
+          "86.168.192.in-addr.arpa" = routerIp;
+          "." = routerIp;
+        };
+      };
       blocking = {
         blackLists = {
           light = [
@@ -38,7 +57,7 @@
         }
       ];
       clientLookup = {
-        upstream = "192.168.86.1";
+        upstream = routerIp;
       };
       caching = {
         minTime = "5m";
