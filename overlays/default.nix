3a8e0aab49648f409782e039ac9e0f9671cb94da
Josh Kasuboski <josh.kasuboski@gmail.com>
2025-08-13 23:06:09 -0500

    update nix2container

--- overlays/default.nix
+++ overlays/default.nix
@@ -2,21 +2,6 @@
   default = final: prev: {
     snapraid-btrfs = prev.callPackage ../pkgs/snapraid-btrfs.nix {};
     snapraid-btrfs-runner = prev.callPackage ../pkgs/snapraid-btrfs-runner.nix {};
-
-    # Fix skopeo build failure due to vendoring issues
-    # https://github.com/containers/skopeo/issues with vendor/modules.txt being out of sync
-    skopeo = prev.skopeo.overrideAttrs (oldAttrs: {
-      # Let Nix handle vendoring instead of relying on broken vendor directory
-      vendorHash = "sha256-po+HqXSndk8kOMzwpHxM9JG675NTIcqHYcnek8aVQTo=";
-
-      # Remove broken vendor directory to force proper dependency handling
-      postPatch =
-        (oldAttrs.postPatch or "")
-        + ''
-          rm -rf vendor
-        '';
-    });
-
     customplex = prev.plex.override {
       # https://github.com/nixos/nixpkgs/issues/433054
       plexRaw = prev.plexRaw.overrideAttrs (old: rec {
