89860017f5d4c458bdb41445cecc7da180d504e8
Josh Kasuboski <josh.kasuboski@gmail.com>
2025-08-28 18:58:54 -0500

    more zwerg

--- hosts/zwerg/configuration.nix
+++ hosts/zwerg/configuration.nix
@@ -1,4 +1,9 @@
-{inputs, ...}: {
+{
+  inputs,
+  outputs,
+  pkgs,
+  ...
+}: {
   imports = [
     inputs.disko.nixosModules.disko
     ./disko-config.nix
@@ -9,12 +14,25 @@
     ../../users/josh
   ];
 
+  nixpkgs.overlays = [outputs.overlays.default];
+  nixpkgs.config.allowUnfree = true;
+
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "zwerg";
   networking.useDHCP = true;
   time.timeZone = "America/Chicago";
+
+  environment.systemPackages = with pkgs; [
+    lm_sensors
+    pipes-rs
+  ];
+
+  services.netdata.enable = true;
+  # https://github.com/NixOS/nixpkgs/issues/402135
+  services.netdata.package = pkgs.netdataCloud;
+  networking.firewall.allowedTCPPorts = [19999];
 
   services.openssh = {
     enable = true;
@@ -26,6 +44,8 @@
   };
 
   fileSystems."/persist".neededForBoot = true;
+
+  users.mutableUsers = false;
 
   system.stateVersion = "25.05";
 }
