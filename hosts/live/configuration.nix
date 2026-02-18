5a19f983208eb83e84e1df8998f36e6ee810fba1
Josh Kasuboski <josh.kasuboski@gmail.com>
2026-02-17 21:20:00 -0600

    update live target

--- hosts/live/configuration.nix
+++ hosts/live/configuration.nix
@@ -1,7 +1,6 @@
 # This is to make an installer iso
 {
   inputs,
-  config,
   pkgs,
   ...
 }: {
@@ -67,5 +66,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "25.11"; # Did you read the comment?
 }
