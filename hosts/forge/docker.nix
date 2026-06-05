1ac1f5a166034a173172726a489f6db35cec0087
Josh Kasuboski <josh.kasuboski@gmail.com>
2026-06-05 10:41:25 -0500

    ollama for nvidia;amd ded

--- hosts/forge/docker.nix
+++ hosts/forge/docker.nix
@@ -9,22 +9,13 @@
   hardware.nvidia-container-toolkit.enable = true;
 
   virtualisation.oci-containers.containers = {
-    "ollama-amd" = {
-      image = "ollama/ollama:rocm";
-      ports = ["11434:11434"];
-      devices = [
-        "/dev/kfd"
-        "/dev/dri"
-      ];
-      volumes = ["ollama:/root/.ollama"];
-    };
-    "unsloth" = {
-      image = "unsloth/unsloth";
-      ports = ["8000:8000" "8888:8888"];
+    "ollama-nvidia" = {
+      image = "ollama/ollama";
+      ports = ["11435:11434"];
       devices = [
         "nvidia.com/gpu=all"
       ];
-      volumes = ["unsloth-work:/unsloth/work" "unsloth-studio:/unsloth/studio"];
+      volumes = ["ollama:/root/.ollama"];
     };
   };
 }
